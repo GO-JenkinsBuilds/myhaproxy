738c343658b79105818b02e7b623a91b9e269301
Paul Malinowski <paulmal@cdw.com>
2017-11-03 13:48:43 -0500

    wsdgwer

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,18 +5,32 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 
-include_recipe 'haproxy::manual'
+# node.default['haproxy']['members'] = [
+# {
+# 'hostname' => 'ec2-184-72-137-102.compute-1.amazonaws.com',
+# 'ipaddress' => '184.72.137.102',
+# 'port' => 80,
+# 'ssl_port' => 80
+# },{
+# 'hostname' => 'ec2-34-229-245-96.compute-1.amazonaws.com',
+# 'ipaddress' => '34.229.245.96',
+# 'port' => 80,
+# 'ssl_port' => 80
+# }]
 
-node.default['haproxy']['members'] = [
-{
-'hostname' => 'localhost',
-'ipaddress' => '127.0.0.1',
-'port' => 4000,
-'ssl_port' => 4000
-},{
-'hostname' => 'ec2-184-72-137-102.compute-1.amazonaws.com',
-'ipaddress' => '184.72.137.102',
+
+
+all_web_nodes = search('node','role:web_server')
+members = []
+all_web_nodes.each do |web_node|
+member = {
+'hostname' => web_node['cloud']['public_hostname'],
+'ipaddress' => web_node['cloud']['public_ipv4'],
 'port' => 80,
 'ssl_port' => 80
-}]
+}
+members.push(member)
+end
+node.default['haproxy']['members'] = members
+
 include_recipe 'haproxy::manual'

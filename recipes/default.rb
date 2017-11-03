7dc9f2dd68562feeb9863c71490f29a608f8eda6
Paul Malinowski <paulmal@cdw.com>
2017-11-03 09:36:18 -0500

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,20 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+include_recipe 'haproxy::manual'
+
+node.default['haproxy']['members'] = [
+{
+'hostname' => 'localhost',
+'ipaddress' => '127.0.0.1',
+'port' => 4000,
+'ssl_port' => 4000
+},{
+'hostname' => 'ec2-34-229-245-96.compute-1.amazonaws.com',
+'ipaddress' => '34.229.245.96',
+'port' => 80,
+'ssl_port' => 80
+}]
+include_recipe 'haproxy::manual'
